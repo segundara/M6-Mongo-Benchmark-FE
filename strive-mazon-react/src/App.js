206c24e19c1954c736abafd046cdeb4100c614b4
segundara <segundara@gmail.com>
2020-07-11 13:05:24 +0300

    updated app js

--- strive-mazon-react/src/App.js
+++ strive-mazon-react/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/TopNav';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Homepage from './components/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navbar />
+        <Route path="/" exact component={Homepage} />
+        <Footer />
+      </Router>
   );
 }
 
