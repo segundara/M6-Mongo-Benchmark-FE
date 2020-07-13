d46ef807739ae7fa3243dcd087a83f50d01b5516
segundara <segundara@gmail.com>
2020-07-13 15:59:26 +0300

    update to app js file

--- strive-mazon-react/src/App.js
+++ strive-mazon-react/src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import Navbar from './components/TopNav';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Homepage from './components/MainPage';
+import Details from './components/Details';
 
-function App() {
-  return (
-      <Router>
-        <Navbar />
-        <Route path="/" exact component={Homepage} />
-        <Footer />
-      </Router>
-  );
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+
+  addProductToCart = (count) => {
+    this.setState({count})
+  }
+
+  render(){
+    return (
+        <Router>
+          <Navbar updateProductInCart={this.state.count}/>
+          <Route path="/" exact render={(props) => <Homepage {...props} addToCart={this.addProductToCart} />} />
+          <Route path="/productDetails/:id" component={Details} />
+          {/* <Footer /> */}
+        </Router>
+    );
+  }
 }
+
 
 export default App;
