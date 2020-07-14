8b6b7fa656eebdd7c46864bf912bfe0ed327c92b
segundara <segundara@gmail.com>
2020-07-14 13:13:12 +0300

    added checkout

---
+++ strive-mazon-react/src/components/Checkout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Checkout() {
+    return (
+        <div>
+            <h4>Checkout page still in progress!</h4>
+        </div>
+    )
+}
+
+export default Checkout
